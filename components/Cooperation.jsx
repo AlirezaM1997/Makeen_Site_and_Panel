b6a77cc51a5b651a39472b8a0dd0c0ac4927988d
Alireza <armahmodi7@gmail.com>
2022-07-15 12:02:46 +0430

    working on cooperation page

--- components/Cooperation.jsx
+++ components/Cooperation.jsx
@@ -24,32 +24,113 @@ const Cooperation = () => {
             </span>
           </div>
         </header>
-        <section  className="px-[244.5px]">
+        <section className="px-[244.5px]">
           <div className="mt-[40px]">
-            <h1 className="text-[18px] text-[#4D6982] font-iranYekan leading-[40px] font-extrabold text-center">!همکار گرامی با سلام</h1>
+            <h1 className="text-[18px] text-[#4D6982] font-iranYekan leading-[40px] font-extrabold text-center">
+              !همکار گرامی با سلام
+            </h1>
             <h3 className="text-[16px] text-[#4D6982] font-iranYekan leading-[40px] font-bold text-center my-5">
               لطفا در صورتی که برای تکمیل تیم خود درخواست جذب نیرو از آکادمی
               مکین را دارید فرم زیر را تکمیل کنید
-              <br/>
-               همکاران ما در اولین فرصت با
-              شما تماس خواهند گرفت
+              <br />
+              همکاران ما در اولین فرصت با شما تماس خواهند گرفت
             </h3>
-            <hr/>
+            <hr />
           </div>
-          <div dir="rtl" className="grid grid-cols-2 grid-rows-5 justify-items-center items-center">
-
-            <div>
-              <label>نام شرکت</label>
-              <input type='text'/>
+          <div
+            dir="rtl"
+            className="grid grid-cols-2 grid-rows-5 gap-[30px] justify-items-center items-center mt-[50px]"
+          >
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+                نام شرکت
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="نام شرکت خود را وارد کنید"
+              />
             </div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              نام نماینده شرکت
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="نام نماینده شرکت را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              سال تاسیس شرکت
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="سال تاسیس شرکت را به صورت 1300/01/1 وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              شماره تماس
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="شماره تماس خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              متقاضی استخدام برای
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="نام شرکت خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              عنوان کاری شرکت
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="عنوان کاری شرکت خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              تعداد پرسنل
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="تعداد پرسنل شرکت خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              تعداد نیروی استخدامی
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="تعداد نیروی مورد نظر خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col items-start w-full">
+              <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
+              نحوه ی آشنایی با بوت کمپ مکین
+              </label>
+              <input
+                type="text"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                placeholder="نام شرکت خود را وارد کنید"
+              />
+            </div>
           </div>
         </section>
       </div>
