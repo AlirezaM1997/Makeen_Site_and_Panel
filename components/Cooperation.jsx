63f69545c6719c27f996df6bca71b6b3a8508374
Alireza <armahmodi7@gmail.com>
2022-07-15 20:38:56 +0430

    working on cooperation page

--- components/Cooperation.jsx
+++ components/Cooperation.jsx
@@ -3,7 +3,48 @@ import Head from "next/head";
 import Image from "next/image";
 
 //form
+import Select from "react-select";
 
+const courses = [
+  {
+    value: "React / React Native دوره ی",
+    label: "React / React Native دوره ی",
+  },
+  {
+    value: "Net .دوره ی",
+    label: "Net .دوره ی",
+  },
+  {
+    value: "Vue.js دوره ی",
+    label: "Vue.js دوره ی",
+  },
+  {
+    value: "Python دوره ی",
+    label: "Python دوره ی",
+  },
+  {
+    value: "UI/UX دوره ی",
+    label: "UI/UX دوره ی",
+  },
+];
+const methodOfIntroduction = [
+  {
+    value: "معرفی توسط دیگران",
+    label: "معرفی توسط دیگران",
+  },
+  {
+    value: "توسط پرسنل خودتون",
+    label: "توسط پرسنل خودتون",
+  },
+  {
+    value: "از طریق آگهی سایت های کاریابی",
+    label: "از طریق آگهی سایت های کاریابی",
+  },
+  {
+    value: "راه های دیگر",
+    label: "راه های دیگر",
+  },
+];
 const Cooperation = () => {
   return (
     <>
@@ -53,7 +94,7 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              نام نماینده شرکت
+                نام نماینده شرکت
               </label>
               <input
                 type="text"
@@ -63,7 +104,7 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              سال تاسیس شرکت
+                سال تاسیس شرکت
               </label>
               <input
                 type="text"
@@ -73,7 +114,7 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              شماره تماس
+                شماره تماس
               </label>
               <input
                 type="text"
@@ -83,17 +124,21 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              متقاضی استخدام برای
+                متقاضی استخدام برای
               </label>
-              <input
-                type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
-                placeholder="نام شرکت خود را وارد کنید"
+              <Select
+                menuPlacement="bottom"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none"
+                placeholder="دوره ی مورد نظر خود را انتخاب کنید"
+                //value={}
+                // onChange={}
+                options={courses}
+                isSearchable={false}
               />
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              عنوان کاری شرکت
+                عنوان کاری شرکت
               </label>
               <input
                 type="text"
@@ -103,7 +148,7 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              تعداد پرسنل
+                تعداد پرسنل
               </label>
               <input
                 type="text"
@@ -113,7 +158,7 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              تعداد نیروی استخدامی
+                تعداد نیروی استخدامی
               </label>
               <input
                 type="text"
@@ -123,14 +168,26 @@ const Cooperation = () => {
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
-              نحوه ی آشنایی با بوت کمپ مکین
+                نحوه ی آشنایی با بوت کمپ مکین
               </label>
-              <input
-                type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
-                placeholder="نام شرکت خود را وارد کنید"
+              <Select
+                menuPlacement="bottom"
+                className="border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none"
+                options={methodOfIntroduction}
+                defaultValue={{
+                  label: "معرفی توسط دیگران",
+                  value: "معرفی توسط دیگران",
+                }}
+                //value={}
+                // onChange={}
+                isSearchable={false}
               />
             </div>
+          </div>
+          <div className="flex justify-center mt-[65px]">
+            <button className="bg-[#00294D] text-[#ECF9FD] py-[14px] px-[51px] rounded-[10px] text-[16px] font-iranYekan leading-[28px] font-bold">
+              ثبت درخواست
+            </button>
           </div>
         </section>
       </div>
@@ -138,6 +195,19 @@ const Cooperation = () => {
         {`
           .bgHeader{
             filter:brightness(70%) blur(8px) opacity(0.8) contrast(1.2) drop-shadow(2px 4px 6px black);
+          }
+          .css-1s2u09g-control , .css-1pahdxg-control{
+            padding: 8px;
+            background-color: transparent !important;
+            border-color:transparent !important;
+            border-width: 0 !important;
+            border-radius: 0 !important;
+            box-shadow: none !important;
+          }
+          .css-1pahdxg-control:focus , .css-1pahdxg-control:hover , .css-1pahdxg-control:active{
+            border-color:transparent !important;
+            border-width: 0 !important;
+            box-shadow: none !important;
           }
           `}
       </style>
