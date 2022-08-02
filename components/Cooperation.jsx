5d5803d63ae2687274adcb94aea7f5e87c2addeb
Alireza <armahmodi7@gmail.com>
2022-08-03 00:00:04 +0430

    working on cooperation form

--- components/Cooperation.jsx
+++ components/Cooperation.jsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 const courses = [
   {
@@ -42,7 +45,48 @@ const methodOfIntroduction = [
     label: "راه های دیگر",
   },
 ];
-const Cooperation = () => {
+export default function Cooperation () {
+  Yup.addMethod(Yup.string, "stripEmptyString", function () {
+    return this.transform((value) => (value === "" ? undefined : value));
+  });
+  const validationSchema = Yup.object().shape({
+    companyName: Yup.string("لطفا نام شرکت خود را وارد کنید").required(
+      "لطفا نام شرکت خود را وارد کنید"
+    ),
+    representative: Yup.string(
+      "لطفا نام نماینده شرکت خود را وارد کنید"
+    ).required("لطفا نام نماینده شرکت خود را وارد کنید"),
+    establishedYear: Yup.string("لطفا سال تاسیس شرکت را وارد کنید")
+      .required("لطفا سال تاسیس شرکت را وارد کنید")
+      .matches(
+        /^\d{4}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])$/,
+        "سال تاسیس شرکت را به صورت 1300/01/1 وارد کنید"
+      ),
+    phone: Yup.string("لطفا شماره تماس خود را وارد کنید")
+      .required("لطفا شماره تماس خود را وارد کنید")
+      .min(11, "لطفا شماره تماس صحیح وارد نمایید")
+      .max(11, "لطفا شماره تماس صحیح وارد نمایید"),
+    workingTitle: Yup.string("لطفا عنوان کاری شرکت را وارد کنید").required(
+      "لطفا عنوان کاری شرکت را وارد کنید"
+    ),
+    numberOfPersonnel: Yup.string("لطفا تعداد پرسنل خود را وارد کنید").required(
+      "لطفا تعداد پرسنل خود را وارد کنید"
+    ),
+    numberOfEmployees: Yup.string(
+      "لطفا تعداد نیروی مورد نظر خود را وارد کنید"
+    ).required("لطفا تعداد نیروی مورد نظر خود را وارد کنید"),
+    jobApplicant: Yup.string().required("لطفا یک مورد را انتخاب کنید"),
+    methodOfIntroduction: Yup.string()
+      .required("لطفا یک مورد را انتخاب کنید")
+      .stripEmptyString()
+      .default("معرفی توسط دیگران"),
+  });
+  const { register, handleSubmit, control, reset, formState, setValue } =
+    useForm({
+      resolver: yupResolver(validationSchema),
+    });
+  const { errors } = formState;
+  const onSubmit = (data) => console.log(data);
   return (
     <>
       <Head>
@@ -62,7 +106,7 @@ const Cooperation = () => {
             </span>
           </div>
         </header>
-        <section className="px-[244.5px]">
+        <form className="px-[244.5px]" onSubmit={handleSubmit(onSubmit)}>
           <div className="mt-[40px]">
             <h1 className="text-[18px] text-[#4D6982] font-iranYekan leading-[40px] font-extrabold text-center">
               !همکار گرامی با سلام
@@ -85,9 +129,15 @@ const Cooperation = () => {
               </label>
               <input
                 type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.companyName ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("companyName")}
                 placeholder="نام شرکت خود را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.companyName?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -95,9 +145,15 @@ const Cooperation = () => {
               </label>
               <input
                 type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.representative ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("representative")}
                 placeholder="نام نماینده شرکت را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.representative?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -105,9 +161,15 @@ const Cooperation = () => {
               </label>
               <input
                 type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.establishedYear ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("establishedYear")}
                 placeholder="سال تاسیس شرکت را به صورت 1300/01/1 وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.establishedYear?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -115,23 +177,46 @@ const Cooperation = () => {
               </label>
               <input
                 type="number"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.phone ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("phone")}
                 placeholder="شماره تماس خود را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.phone?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
                 متقاضی استخدام برای
               </label>
-              <Select
-                menuPlacement="bottom"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none"
-                placeholder="دوره ی مورد نظر خود را انتخاب کنید"
-                //value={}
-                // onChange={}
-                options={courses}
-                isSearchable={false}
+              <Controller
+                control={control}
+                name="jobApplicant"
+                render={({ field: { onChange } }) => (
+                  <Select
+                    menuPlacement="bottom"
+                    name="jobApplicant"
+                    {...register("jobApplicant")}
+                    placeholder="دوره ی مورد نظر خود را انتخاب کنید"
+                    options={courses}
+                    onChange={(e) => {
+                      onChange(e);
+                      setValue("jobApplicant", e.value, {
+                        shouldValidate: true,
+                      });
+                    }}
+                    className={`${
+                      errors.jobApplicant ? "border-[#B80000]" : ""
+                    } border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none`}
+                    isSearchable={false}
+                  />
+                )}
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.jobApplicant?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -139,9 +224,15 @@ const Cooperation = () => {
               </label>
               <input
                 type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.workingTitle ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("workingTitle")}
                 placeholder="عنوان کاری شرکت خود را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.workingTitle?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -149,9 +240,15 @@ const Cooperation = () => {
               </label>
               <input
                 type="number"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.numberOfPersonnel ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("numberOfPersonnel")}
                 placeholder="تعداد پرسنل شرکت خود را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.numberOfPersonnel?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
@@ -159,34 +256,57 @@ const Cooperation = () => {
               </label>
               <input
                 type="text"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none"
+                className={`${
+                  errors.numberOfEmployees ? "border-[#B80000]" : ""
+                } border-[#CACBCB] border-[1px] rounded-[10px] p-[15px] w-full focus:outline-none`}
+                {...register("numberOfEmployees")}
                 placeholder="تعداد نیروی مورد نظر خود را وارد کنید"
               />
+              <span className="text-[10px] text-[#B80000] leading-[36px] font-bold -mt-[5px] -mb-8">
+                {errors.numberOfEmployees?.message}
+              </span>
             </div>
             <div className="flex flex-col items-start w-full">
               <label className="text-[14px] text-[#00213E] font-iranYekan leading-[36px] font-bold">
                 نحوه ی آشنایی با بوت کمپ مکین
               </label>
-              <Select
-                menuPlacement="bottom"
-                className="border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none"
-                options={methodOfIntroduction}
-                defaultValue={{
-                  label: "معرفی توسط دیگران",
-                  value: "معرفی توسط دیگران",
-                }}
-                //value={}
-                // onChange={}
-                isSearchable={false}
+              <Controller
+                control={control}
+                name="methodOfIntroduction"
+                render={({ field: { onChange } }) => (
+                  <Select
+                    menuPlacement="bottom"
+                    name="methodOfIntroduction"
+                    {...register("methodOfIntroduction")}
+                    options={methodOfIntroduction}
+                    defaultValue={{
+                      label: "معرفی توسط دیگران",
+                      value: "معرفی توسط دیگران",
+                    }}
+                    onChange={(e) => {
+                      onChange(e);
+                      setValue("methodOfIntroduction", e.value, {
+                        shouldValidate: true,
+                      });
+                    }}
+                    className={`${
+                      errors.methodOfIntroduction ? "border-[#B80000]" : ""
+                    } border-[#CACBCB] border-[1px] rounded-[10px] w-full focus:outline-none`}
+                    isSearchable={false}
+                  />
+                )}
               />
             </div>
           </div>
-          <div className="flex justify-center mt-[65px]">
-            <button className="bg-[#00294D] text-[#ECF9FD] py-[14px] px-[51px] rounded-[10px] text-[16px] font-iranYekan leading-[28px] font-bold">
+          <div className="flex justify-center">
+            <button
+              className="bg-[#00294D] text-[#ECF9FD] py-[14px] px-[51px] rounded-[10px] text-[16px] font-iranYekan leading-[28px] font-bold mt-[65px] hover:mt-[59px] hover:mb-[6px] transition-spacing duration-300 ease-out"
+              type="submit"
+            >
               ثبت درخواست
             </button>
           </div>
-        </section>
+        </form>
       </div>
       <style>
         {`
@@ -198,5 +318,3 @@ const Cooperation = () => {
     </>
   );
 };
-
-export default Cooperation;
