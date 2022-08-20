9b19d979d90f609311afe8a06a86a2272afa30cb
Alireza <armahmodi7@gmail.com>
2022-08-20 11:43:28 +0430

    working on courses page

--- components/site/courses/CourseCard.jsx
+++ components/site/courses/CourseCard.jsx
@@ -15,27 +15,28 @@ export const CourseCard = ({ index }) => {
   };
   return (
     <>
-      <div className="relative w-full">
+      <div className="relative">
         <li
-          onClick={() => showAndHideToggle(index)}
-          key={index}
           className={`${
             showAndHideCourse === index
-              ? "z-30 h-[500px] absolute"
+              ? "z-30 h-auto absolute"
               : "relative h-[395px]"
-          }  pt-[30px] pb-[25px] px-[30px] flex flex-col w-full items-center bg-white shadow-[10px_10px_20px_#00000014] rounded-[20px] border-[0.5px] border-[#00000014] cursor-pointer`}
+          }  flex flex-col items-center bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CACBCB] py-[25px] px-[20px] overflow-hidden transition-height duration-300 ease-out`}
+          key={index}
         >
-          <div className="relative w-[100px] h-[115px] top-0 group-hover:-top-[20px] transition-top duration-300 ease-out">
-            <Image
-              src="/assets/images/course-ui.png"
-              layout="fill"
-              alt="course"
-            />
+          <div className="h-full w-full flex justify-center">
+            <div className="relative w-[100px] h-[115px]">
+              <Image
+                src="/assets/images/course-ui.png"
+                layout="fill"
+                alt="course"
+              />
+            </div>
           </div>
           <div className="text-[#00213E] text-[18px] leading-[36px] font-bold mt-[20px] mb-[11px]">{`UI/UX دوره`}</div>
           <div className="text-[#4D6982] text-[14px] leading-[36px] font-medium mb-[11px]">{`امیرحسین آل احمد`}</div>
           <span className="text-center text-[#025D7E] text-[14px] w-[130px] leading-[25px] bg-[#E4F6FC] rounded-[6px] px-[10px] py-1">{`بزودی`}</span>
-          <hr className="my-[30px] w-full" />
+          <hr className="mt-[30px] mb-[25px] w-full" />
           <div
             className={`${
               showAndHideCourse === index ? "hidden" : ""
@@ -46,11 +47,10 @@ export const CourseCard = ({ index }) => {
                 اطلاعات بیشتر
               </a>
             </Link>
-
             <div
               dir="rtl"
               className="flex justify-center items-center cursor-pointer relative"
-              onClick={() => dispatch(showHideModalAction(true))}
+              onClick={() => showAndHideToggle(index)}
             >
               <span className="text-[#00294D] peer text-[16px] leading-[28px] pl-1">
                 ثبت نام دوره
@@ -76,20 +76,17 @@ export const CourseCard = ({ index }) => {
               </svg>
             </div>
           </div>
-
-          <div
-            className={`${
-              showAndHideCourse === index ? "h-[100px]" : "hidden"
-            } `}
-          >
-            <div className="bg-[#EDEEEE] rounded-[10px] flex items-center">
+          <div className={`mt-[35px]`}>
+            <div className="bg-[#EDEEEE] rounded-[10px] flex items-center justify-between px-[10px] py-[15px] w-full mb-5">
               <div className="flex items-center">
-                <div className="relative w-[18px] h-[18px]">
-                  <Image
-                    src="/assets/images/courses/Courselength.png"
-                    layout="fill"
-                    alt="course"
-                  />
+                <div className="rounded-full border-[#707070] border-[1px] border-opacity-20 w-[35px] h-[35px] flex justify-center items-center ml-2">
+                  <div className="relative w-[18px] h-[18px] rounded-full">
+                    <Image
+                      src="/assets/courses/Courselength.png"
+                      layout="fill"
+                      alt="course"
+                    />
+                  </div>
                 </div>
                 <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
                   طول دوره
@@ -99,9 +96,93 @@ export const CourseCard = ({ index }) => {
                 طول دوره
               </span>
             </div>
-
-            <div></div>
-            <div></div>
+            <div className="bg-[#EDEEEE] rounded-[10px] flex items-center justify-between px-[10px] py-[15px] w-full mb-5">
+              <div className="flex items-center">
+                <div className="rounded-full border-[#707070] border-[1px] border-opacity-20 w-[35px] h-[35px] flex justify-center items-center ml-2">
+                  <div className="relative w-[18px] h-[18px] rounded-full">
+                    <Image
+                      src="/assets/courses/Courselength.png"
+                      layout="fill"
+                      alt="course"
+                    />
+                  </div>
+                </div>
+                <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
+                  طول دوره
+                </span>
+              </div>
+              <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
+                طول دوره
+              </span>
+            </div>
+            <div className="bg-[#EDEEEE] rounded-[10px] flex items-center justify-between px-[10px] py-[15px] w-full mb-5">
+              <div className="flex items-center">
+                <div className="rounded-full border-[#707070] border-[1px] border-opacity-20 w-[35px] h-[35px] flex justify-center items-center ml-2">
+                  <div className="relative w-[18px] h-[18px] rounded-full">
+                    <Image
+                      src="/assets/courses/Courselength.png"
+                      layout="fill"
+                      alt="course"
+                    />
+                  </div>
+                </div>
+                <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
+                  طول دوره
+                </span>
+              </div>
+              <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
+                طول دوره
+              </span>
+            </div>
+            <div
+              id={`prerequisite${index}`}
+              className="bg-[#EDEEEE] rounded-[10px] flex items-center justify-between px-[10px] py-[15px] w-full mb-5"
+            >
+              <div className="flex items-center">
+                <div className="rounded-full border-[#707070] border-[1px] border-opacity-20 w-[35px] h-[35px] flex justify-center items-center ml-2">
+                  <div className="relative w-[18px] h-[18px] rounded-full">
+                    <Image
+                      src="/assets/courses/Courselength.png"
+                      layout="fill"
+                      alt="course"
+                    />
+                  </div>
+                </div>
+                <span className="text-[#00213E] text-[14px] leading-[30px] font-bold">
+                  طول دوره
+                </span>
+              </div>
+              <span className="text-[#00213E] text-[14px] leading-[30px] font-bold text-left">
+                تماشای فیلم های مبانی برنامه نویسی
+              </span>
+            </div>
+            <div className="flex justify-center">
+              <button className="py-[14px] px-[73px] bg-[#00294D] rounded-[10px] text-[#ECF9FD] font-bold text-base">
+                ثبت نام
+              </button>
+            </div>
+            <div className="flex justify-center mt-[40px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="5.041"
+                height="8.554"
+                viewBox="0 0 5.041 8.554"
+                className=" transition-all duration-300 ease-out rotate-90 cursor-pointer"
+                onClick={() => showAndHideToggle(index)}
+              >
+                <path
+                  id="Path_58"
+                  dataname="Path 58"
+                  d="M4.229,7.567,1,4.351l3.23-3.217"
+                  transform="translate(-0.249 -0.073)"
+                  fill="none"
+                  stroke="#8094a6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+              </svg>
+            </div>
           </div>
         </li>
       </div>
--- components/site/courses/CoursesPage.jsx
+++ components/site/courses/CoursesPage.jsx
@@ -1,36 +1,50 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 import { CourseCard } from "./CourseCard";
 export default function CoursesPage() {
   const a = [1, 2, 3, 4, 5];
+  const [showAndHideCourse, setShowAndHideCourse] = useState(null);
+  const showAndHideToggle = (index) => {
+    if (showAndHideCourse !== null) {
+      setShowAndHideCourse(null);
+    } else {
+      setShowAndHideCourse(index);
+    }
+  };
   return (
     <>
-      <section className="pt-[140px] px-[50px] font-iranYekan mb-[333px]">
+      <section className="pt-[140px] px-[50px] font-iranYekan mb-[460px]">
         <h1 className="text-[#00213e] text-[20px] leading-[35px] font-extrabold text-center">
           دوره های آکادمی مکین
         </h1>
         <h4 className="text-[#00213E] text-[16px] leading-[28px] font-bold mt-[20px] text-center">
           همه ی دوره هایی که در حال حاضر مکین برگزار می کنه
         </h4>
-        
         <ul
-          className="grid grid-cols-3 gap-[30px] justify-items-center mt-[40px] relative"
+          className="grid grid-cols-3 gap-[30px] w-full px-[30px] relative mt-[40px]"
           dir="rtl"
         >
           {a.map((item, index) => (
-            <CourseCard index={index}/>
+            <CourseCard index={index} />
           ))}
-          <li className="flex flex-col items-center justify-center rounded-[10px] bg-gradient-[134deg] from-[#E6EAED] to-[#8094A6] w-full h-full">
-            <div className="relative w-[35px] h-[44px]">
-              <Image
-                src="/assets/images/new_course_is_coming.png"
-                layout="fill"
-                alt="new_course"
-              />
-            </div>
-            <p className="text-[#00213E] text-[16px] leading-[28px] font-bold mt-[31px] text-center" dir="ltr">
-              !دوره های جدید به زودی اضافه میشن
-            </p>
-          </li>
+          <div>
+            <li className="flex flex-col items-center justify-center rounded-[10px] bg-gradient-[134deg] from-[#E6EAED] to-[#8094A6] w-full h-full">
+              <div className="relative w-[35px] h-[44px]">
+                <Image
+                  src="/assets/images/new_course_is_coming.png"
+                  layout="fill"
+                  alt="new_course"
+                />
+              </div>
+              <p
+                className="text-[#00213E] text-[16px] leading-[28px] font-bold mt-[31px] text-center"
+                dir="ltr"
+              >
+                !دوره های جدید به زودی اضافه میشن
+              </p>
+            </li>
+          </div>
         </ul>
       </section>
     </>
